eb8366ebd3bfdde76edaf4d3abebabe280a7da32
maritheygraine <75174607+maritheygraine@users.noreply.github.com>
2021-10-02 13:35:01 +0800

    #6 table view fixed. added column width

--- TechknowsNotion_1.py
+++ TechknowsNotion_1.py
@@ -2,9 +2,9 @@ import os, csv, io
 from os import path
 import os
 import pandas as pd
-from Tools.scripts.dutree import display
 
-list1 = []                                                                  # list of all projects
+list1 = []  # list of all projects
+
 
 class Preview:
     def __init__(self, lst1=[]):
@@ -20,14 +20,13 @@ class Preview:
 class OtherFunctions:
     def iteration(self, filename):
         data = pd.read_csv(filename)
-
         df = pd.DataFrame(data)
         left_aligned_df = df.style.set_properties(**{'text-align': 'left'})
-        display(self, left_aligned_df)
-
+        left_aligned_df_width = df.style.set_properties(**{'width': '100px'})
+        display(left_aligned_df_width)
 
     def is_empty(self, x):
-        with open(x, 'r') as csvfile:                                       # check queue if empty or not
+        with open(x, 'r') as csvfile:  # check queue if empty or not
             csv_dict = [row for row in csv.DictReader(csvfile)]
             if len(csv_dict) == 0:
                 self.o = False
@@ -83,7 +82,8 @@ class OtherFunctions:
             while True:
                 try:
                     i = input(x)
-                    if i == " " or i == "" : raise ValueError
+                    if i == " " or i == "":
+                        raise ValueError
                     else:
                         return i
                 except ValueError:
@@ -94,6 +94,7 @@ class OtherFunctions:
 
 def clear():
     os.system('cls')
+
 
 # Techknow's Notion Main Menu
 def main():
@@ -128,23 +129,23 @@ def input_module_details():
     print('\tINPUT MODULE DETAILS\n')
     while True:
         p = OtherFunctions()
-        module_name = p.input_validation('Modules: ',2)
-        type = p.input_validation('Type: ',2)
-        tasks = p.input_validation('No. of Tasks: ',1)
-        priority = p.input_validation('Priority: ',1)
+        module_name = p.input_validation('Modules: ', 2)
+        type = p.input_validation('Type: ', 2)
+        tasks = p.input_validation('No. of Tasks: ', 1)
+        priority = p.input_validation('Priority: ', 1)
         with open("all_modules.csv", "r") as f:
             csvreader = csv.reader(f, delimiter=",")
             for row in csvreader:
-                list1.append(row[0])                                        # insert all_project data column to list
-            for i in list1:                                                 # iterate data
-                if i == module_name:                                        # check if module is in data
+                list1.append(row[0])  # insert all_project data column to list
+            for i in list1:  # iterate data
+                if i == module_name:  # check if module is in data
                     marker = 1
                 else:
                     pass
-            if marker == 1:                                                 # if module name exist
+            if marker == 1:  # if module name exist
                 print("\nModules Exist")
                 p.try_again('Make new module details?', 'Invalid input.', 1)
-            else:                                                           # data is not located : add new data to file
+            else:  # data is not located : add new data to file
                 with open("all_modules.csv", 'a', newline='\n') as f1, open("queue.csv", 'a', newline='\n') as f2:
                     f1writer = csv.writer(f1)
                     f1writer.writerow([module_name, type, tasks, priority])
@@ -168,21 +169,22 @@ def view_module():
             print('Invalid input.', '\n')
         else:
             break
-    if choice == 1:                                                         # all completed modules
+    if choice == 1:  # all completed modules
         clear()
-        print('\t     VIEW MODULES')
-        print('    LIST OF ALL COMPLETED MODULES\n')
+        print('\t\t     VIEW MODULES')
+        print('\t    LIST OF ALL COMPLETED MODULES\n')
         p = OtherFunctions()
         if p.is_empty('completed_modules.csv') == True:
-            p.iteration('completed_modules.csv')                            # display the data
+            p.iteration('completed_modules.csv')  # display the data
+
             p.try_again('View module again?', 'Invalid input.', 2)
         else:
             print("\tNo completed module yet")
             p.try_again('View module again?', 'Invalid input.', 2)
-    elif choice == 2:                                                       # all module
+    elif choice == 2:  # all module
         clear()
-        print('\t     VIEW MODULES')
-        print('\t     ALL MODULES\n')
+        print('\t\t     VIEW MODULES')
+        print('\t\t     ALL MODULES\n')
         p = OtherFunctions()
         if p.is_empty('all_modules.csv') == True:
             p.iteration('all_modules.csv')
@@ -211,29 +213,29 @@ def schedule_module():
     p = OtherFunctions()
     if choice == 1:
         clear()
-        print('\t     VIEW MODULES')
-        print('    VIEW SCHEDULE UPDATED SCHEDULE\n')
+        print('\t\t     VIEW MODULES')
+        print('\t    VIEW SCHEDULE UPDATED SCHEDULE\n')
         if p.is_empty('queue.csv') == True:
             queue_projects = open('queue.csv', 'r')
-            data = csv.DictReader(queue_projects)                           # read the file and separated the value
-            mylist = []                                                     # as dictionary key
+            data = csv.DictReader(queue_projects)  # read the file and separated the value
+            mylist = []  # as dictionary key
             for row in data:
                 single_list = []
-                single_list.append(row['Modules'])                          # appends the column under the header
+                single_list.append(row['Modules'])  # appends the column under the header
                 single_list.append(row['Type'])
-                single_list.append(int(row['No. of Tasks']))                # typecast the value into integer
+                single_list.append(int(row['No. of Tasks']))  # typecast the value into integer
                 single_list.append(int(row['Priority']))
-                mylist.append(single_list)                                  # store the data into the list
+                mylist.append(single_list)  # store the data into the list
 
-            sorted_queue1 = (sorted(mylist, key=lambda v: (v[2], v[2])))         # sort the data by the sizes
+            sorted_queue1 = (sorted(mylist, key=lambda v: (v[2], v[2])))  # sort the data by the sizes
             sorted_queue2 = (sorted(sorted_queue1, key=lambda v: (v[3], v[2])))  # sort the data by the priority
 
-            with open('queue.csv', 'w', newline='') as queue_new:                # displaying the header
+            with open('queue.csv', 'w', newline='') as queue_new:  # displaying the header
                 queue_writer = csv.DictWriter(queue_new, delimiter=',', fieldnames=['Modules', 'Type', 'No. of Tasks',
                                                                                     'Priority'])
                 queue_writer.writeheader()
                 queue_writer = csv.writer(queue_new)
-                queue_writer.writerows(sorted_queue2)                       # writes the sorted items in the csv
+                queue_writer.writerows(sorted_queue2)  # writes the sorted items in the csv
             queue_new.close()
 
             file3 = open('queue.csv', 'r')
@@ -255,10 +257,10 @@ def get_a_module():
     p = OtherFunctions()
     clear()
     a = False
-    print('\t      GET A MODULE\n')
+    print('\t\t    GET A MODULE\n')
     if p.is_empty('queue.csv') == True:
         a = True
-        p.iteration('queue.csv')                                            # display the data
+        p.iteration('queue.csv')  # display the data
         while True:
             try:
                 choice = int(input('\nMark as complete the topmost module? YES(1) or NO(0): '))
@@ -273,31 +275,31 @@ def get_a_module():
                 lines = []
                 with open('queue.csv', newline='') as f:
                     reader = csv.reader(f)
-                    row1 = next(reader)                                     # header
-                    row2 = next(f)                                          # get the first row
-                list5 = row2.split(",")                                     # put the first row in list
-                list6 = [i.strip() for i in list5]                          # removing \n \t
+                    row1 = next(reader)  # header
+                    row2 = next(f)  # get the first row
+                list5 = row2.split(",")  # put the first row in list
+                list6 = [i.strip() for i in list5]  # removing \n \t
                 with open('queue.csv', 'r') as readFile:
                     reader = csv.reader(readFile)
                     for row in reader:
-                        lines.append(row)                                   # add data row in list - lines
-                        for field in row:                                   # iterate the row
-                            if field == list6[0]:                           # check if module of first row(list6) if same
-                                lines.pop()                                 # remove line
+                        lines.append(row)  # add data row in list - lines
+                        for field in row:  # iterate the row
+                            if field == list6[0]:  # check if module of first row(list6) if same
+                                lines.pop()  # remove line
                                 break
                 with open('queue.csv', 'w', newline='') as writeFile:
                     writer = csv.writer(writeFile)
-                    writer.writerows(lines)                                 # rewrite csv file with the list - lines
+                    writer.writerows(lines)  # rewrite csv file with the list - lines
                 file2 = open("completed_modules.csv", 'a', newline='')
                 awriter = csv.writer(file2)
-                awriter.writerow(list6)                                     # add first row(list5) to completed projects
+                awriter.writerow(list6)  # add first row(list5) to completed projects
                 file2.close()
                 print("---Module complete---")
                 file1 = open('queue.csv', 'r')
                 lines = file1.readlines()
                 file1.close()
                 print()
-                p.iteration('queue.csv')                                    # display updated queue"""
+                p.iteration('queue.csv')  # display updated queue"""
             else:
                 print("Queue is empty")
         elif choice == 0:
@@ -306,7 +308,6 @@ def get_a_module():
     else:
         print("\t\tQueue is empty")
         p.restart()
-
 
 
 def make_csv_files():
